142c68796115224b553e1aa46834427e991acf15
lrbtech <info@lrbtech>
2019-01-20 22:07:46 +0530

    PRoduct 20-1-19

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('home');
 });
+Route::group(['prefix' => 'admin'],function(){
+Route::get('/login', function () {
+    return view('admin/app');
+});
+
+//brands
+Route::get('/brand','productController@viewBrand');
+Route::get('/edit_brand/{id}','productController@editBrand');
+Route::get('/delete_brand/{id}','productController@deleteBrand');
+Route::post('/add-brand','productController@brandStore');
+Route::post('/update-brand','productController@brandUpdate');
+
+// Attribute Management
+Route::get('/attribute','productController@viewAttribute'); 
+Route::post('/attributeSave','productController@attributeSave');
+Route::post('/attributeUpdate','productController@attributeUpdate');
+Route::get('/attributeEdit/{id}','productController@attributeEdit');
+Route::get('/attributeDelete/{id}','productController@attributeDelete');
+
+// Terms Management 
+Route::get('/terms/{id}','productController@viewTerms'); 
+Route::post('/termsSave','productController@termsSave');
+Route::post('/termsUpdate','productController@termsUpdate');
+Route::get('/termsEdit/{id}','productController@termsEdit');
+Route::get('/termsDelete/{id}','productController@termsDelete');
+
+
+// Product Group Management
+Route::get('/product-group','productController@productGroup'); 
+Route::post('/saveGroup','productController@saveGroup');
+Route::post('/updateGroup','productController@updateGroup');
+Route::get('/editGroup/{id}','productController@editGroup');
+Route::get('/deleteGroup/{id}','productController@deleteGroup');
+
+// Category Management
+Route::get('/category','productController@viewCategory'); 
+Route::get('/category/{id}','productController@viewCategoryId'); 
+});
